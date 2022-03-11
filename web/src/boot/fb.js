429193e2394d04da6905869922748e90a199cb6a
lipjae <dlwognscap@gmail.com>
2022-03-11 11:33:23 +0900

    update firebase

---
+++ web/src/boot/fb.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9TIlz21CXTo59dihp_7XZ52XNgX6u8sA",
+  authDomain: "medical-data-dev.firebaseapp.com",
+  projectId: "medical-data-dev",
+  storageBucket: "medical-data-dev.appspot.com",
+  messagingSenderId: "376031364074",
+  appId: "1:376031364074:web:bb090a70875542d8dd2893",
+  measurementId: "G-22JD472DCV"
+};
+
+
+const auth = firebase.auth()
+
+export { auth }
